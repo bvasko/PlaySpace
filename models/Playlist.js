c7a17d2a5efb1fe868ac3b3e16334703c2e48060
“Lauren <lferrett@gmail.com>
2022-01-27 14:03:56 -0500

    Updated Playlist routes, model, and sample seed data in json format.

---
+++ models/Playlist.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Playlist extends Model {}
+
+Playlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comments: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    upvotes: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    dateCreated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    favorited: {
+      type: DataTypes.BOOLEAN,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Playlist',
+  }
+);
+
+module.exports = Playlist;
