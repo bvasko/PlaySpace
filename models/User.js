92065a4ae7fb4fdaccdbaa6fc984e1f39a49f3c7
Tina Stanczyk <cstanczyk123@gmail.com>
2022-01-27 11:58:42 -0500

    created user model

--- models/User.js
+++ models/User.js
@@ -10,13 +10,39 @@ class User extends Model {
 
 User.init(
   {
-    id:
-    name:
-    email:
-    password:
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
   },
   {
-    hooks: {},
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps,
     freezeTableName: true,
