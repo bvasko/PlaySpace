e2266e746e50e00e33f4c2fa527bd19d18b2e7dd
Tina Stanczyk <cstanczyk123@gmail.com>
2022-01-27 13:23:35 -0500

    working on user auth

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,11 +3,7 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create({
-      name = req.body.name,
-      email = req.body.email,
-      password = req.body.password,
-    });
+    const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
