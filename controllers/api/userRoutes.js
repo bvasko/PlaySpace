c35a4b7aa34b51bc5519af8970fe1fe52eca6ce6
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-01-30 11:41:56 -0500

    each playllist on dashboard shows the user who created

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.get('/:id', async (req,res) => {
-try {
-  const userData = await User.findByPk(req.params.id);
-  res.status(200).json(userData.name);
-} catch (err) {
-  res.status(500).json(err);
-}
-});
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
