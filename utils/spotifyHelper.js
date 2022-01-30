e3dde217336a31a2a622630e4aa3736e65beb651
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-30 18:41:06 -0500

    add filter and hover effect to playlist covers

--- utils/spotifyHelper.js
+++ utils/spotifyHelper.js
@@ -1,5 +1,5 @@
 const baseSpotifyAPI = 'https://api.spotify.com/v1';
-const PLAYLIST_URL = `${baseSpotifyAPI}/me/playlists?limit=20`;
+const PLAYLIST_URL = `${baseSpotifyAPI}/me/playlists?limit=30`;
 
 
 module.exports = {PLAYLIST_URL}
