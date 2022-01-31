608631d42e7667770ddaceddb68d5a895bf12823
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-31 17:00:30 -0500

    remove console.log

--- public/js/playlistTransfer.js
+++ public/js/playlistTransfer.js
@@ -4,8 +4,6 @@ const queryArray = new URLSearchParams(queryStr);
 const token = queryArray.get('access_token');
 localStorage.setItem('access_token', token);
 
-console.log('#', )
-
 const transferPlaylist = async function(body, cb) {
   try {
     const playlist = await fetch('/api/playlist', {
