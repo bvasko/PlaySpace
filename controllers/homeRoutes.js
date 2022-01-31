3b9281b38d542e70b228c3adaac1a2e3462284af
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-30 19:35:01 -0500

    add list view of user playlists

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -80,6 +80,27 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile/playlists', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Playlist }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('myplaylists', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
